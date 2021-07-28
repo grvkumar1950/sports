fc03d0700002eebdbd1d667137483c039dba7000
grvkumar1950 <88034056+grvkumar1950@users.noreply.github.com>
2021-07-28 08:35:22 +0530

    Add files via upload

---
+++ about-adonis-events.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>About Adonis Events - Destination weddings, event management, birthday parties, corporate events</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+</head>
+<body>
+	<?php include('common/header.php');?>
+	<div class="container">
+		<div class="row" id="top10">
+			<div class="col-sm-12">
+				<div class="col-md-8">
+					<blockquote class="blockquote">Wedding – a meeting of minds and hearts. It is the coming together of two people in the midst of two families rejoicing and traversing the wide range of emotions emerging forth and going around. We understand and revel in creating and being a part of such wonderful and unforgettable moments. ADONIS is the wedding wing .So all you have to do is finalize the bride or the groom, give us a call, then sit back and rejoice in the warmth and happiness of the occasion as we ensure all details are taken care of and worries are smothered over, before they cause even half a frown.</blockquote>
+					<blockquote class="blockquote">Nothing in our range of services is ‘packaged’ – everything that we do we ensure it is fresh, original and memorable for all the right reasons. So why not leave it all in our capable hands – so that your soon to be special ‘Wedding’ can get the perfect touch of ‘ADONIS’. We work in phases like initial consultation, budget setting, selecting suitable destination, designing wedding package, selecting quality vendors, building and weaving a theme throughout the event, from creating first impressions to leaving lasting impressions.Adonis with its past experience and professionalism can make your dream wedding come true. Once we have an idea of your expectation, we groom it with our professional touch in order to turn your dream fairy tale wedding a memorable reality. Our wedding planner stays with you from the starting of the query to the end of your big day, providing you with the best of services and to ensure that everything is planned and committed. Your wedding day is the most important event in your life and we leave no stone un-turned in making it the most memorable one as well.</blockquote>
+				</div>
+				<div class="col-sm-4"  id="about">
+				<h3>FACES OF ADONIS</h3>
+					<div class="thumbnail" id="about">
+						 <!--<h3>FACES OF ADONIS</h3>-->
+						<img src="img/anusha-mody-adonis-events.jpg" alt="Anusha Mody">
+						<h4>-Anusha Mody</h4>
+						
+					</div>
+					<div class="thumbnail" id="about">
+						<img src="img/mayur-adonis-events.jpg" alt="Mayur">
+						<h4>- Mayur</h4>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+	<?php include('common/footer.php');?>
+		
+</body>
+</html>
---
+++ contact-isama.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Taekwondo,karate,kick boxing,MMA,Yoga classes in Pune, Delhi, Mumbai, Bangalore</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+</head>
+<body>
+	<?php include('common/header.php');?>
+
+
+<!----start------>
+<div class="container">
+
+        <div class="row" id="top10" >
+
+            <div class="col-md-6" >
+<h3> Quick Connect Form</h3>
+                <p class="lead">Please leave your message</p>
+
+
+                <form id="contact-form" method="post" action="contact.php" role="form">
+
+                    <div class="messages"></div>
+
+                    <div class="controls">
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="form_name">Full name *</label>
+                                    <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your Full name*" required="required" data-error="Firstname is required.">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="form_lastname">Contact No.*</label>
+                                    <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your contact number*" required="required" data-error="Contact number is required.">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="form_email">Email *</label>
+                                    <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="form_need">Please specify your need *</label>
+                                    <select id="form_need" name="need" class="form-control" required="required" data-error="Please specify your need.">
+                                        <option value=""></option>
+                                        <option value="Request quotation">New Admission </option>
+                                        <option value="Request order status">New Sports Gear</option>
+                                        <option value="Request copy of an invoice">Call Back Request</option>
+                                        <option value="Other">Other</option>
+                                    </select>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="form_message">Message *</label>
+                                    <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please, leave us a message."></textarea>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <input type="submit" class="btn btn-success btn-send" value="Send message">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <p class="text-muted">
+                                    <strong>*</strong> These fields are required. </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>
+            <!-- /.6 -->
+
+
+<div class="col-sm-6">
+<h3 style="text-align:center; border:dotted 1px white; padding:10px 0px; ">Head Office / Main Branch</h3>
+						<div class="thumbnail" style="text-align:left;">
+							<div class="caption" style="margin-left:50px;">
+								<h3>India Sports Academy of Martial Arts</h3>
+								<p><strong>4th floor, Varsha Square, </br>Near Bhairavi Hotel, Baner, Pune-411045</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								<p><Strong>Email:</Strong> connect@sportyindians.com </p>
+<p><Strong>Website:</Strong> www.sportyindians.com </p>
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+</div>
+
+<!------------------------------------------------------------------------->
+
+        </div>
+        <!-- /.row-->
+
+    </div>
+    <!-- /.container-->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js" integrity="sha256-dHf/YjH1A4tewEsKUSmNnV05DDbfGN3g7NMq86xgGh8=" crossorigin="anonymous"></script>
+    <script src="contact.js"></script>
+
+<!----end---->
+
+
+
+
+		<div class="container">
+
+
+			<!--<div class="row" id="top10">
+			<h3 style="text-align:center; border:dotted 2px white; padding:10px 0px; color:#a20202">Head Office / Main Branch</h3>
+
+<div class="col-sm-12">
+						<div class="thumbnail" style="text-align:center">
+							<div class="caption">
+								<h4>India Sports Academy of Martial Arts</h4>
+								<p><strong>4th floor, Varsha Square, Near Bhairavi Hotel, Baner, Pune-411045</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								<p><Strong>Email:</Strong> connect@sportyindians.com</p>
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+</div>
+
+<!------------------------------------------------------------------------->
+
+
+			
+			<h3 style="text-align:center; border:dotted 2px white; padding:10px 0px; color:#a20202">Pune Branches</h3>
+
+				<div class="col-sm-4">
+						<div class="thumbnail">
+							<div class="caption">
+								<h4>India Sports Academy of Martial Arts</h4>
+								<p style="font-size:12px"><strong>Rabindranath Tagore International school, Balewadi, Pune, India</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								<p><Strong>Email:</Strong> connect@sportyindians.com </p>
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+				<div class="col-sm-4">
+						<div class="thumbnail">
+							<div class="caption">
+								<h4>Aundh Branch</h4>
+								<p><strong>Sparkles School, DP Road, Aundh,Pune.</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+				<div class="col-sm-4">
+						<div class="thumbnail">
+							<div class="caption">
+								<h4>Pimple Nilakh Branch</h4>
+								<p><strong>Chaitanya English School, Pimple Nilakh,Pune.</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+                                              <div class="col-sm-4">
+						<div class="thumbnail">
+							<div class="caption">
+								<h4>Baner Branch</h4>
+								<p><strong>Aditya Internatinal School, Pan card club,Baner, Pune.</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+				<div class="col-sm-4">
+						<div class="thumbnail">
+							<div class="caption">
+								<h4>Balewadi Branch</h4>
+								<p><strong>Ecole Heritage School,Balewadi,Pune</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+				
+				<div class="col-sm-4">
+						<div class="thumbnail">
+							<div class="caption">
+								<h4>Upcoming Branches</h4>
+								<p><Strong>Mumbai, Gurgaon, Bangalore</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								
+								
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+				<div class="row" id="top10">
+		</div>
+				<h3 style="text-align:center; border:dotted 2px white; padding:10px 0px; color:#a20202">Delhi Branches</h3>
+				<div class="col-sm-4">
+						<div class="thumbnail">
+							<div class="caption">
+								<h4>Uttam Nagar</h4>
+								<p><Strong>Braodways Public School, Janam Puri, Uttam Nagar, Janak Puri,New Delhi</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+				
+				<div class="col-sm-4">
+						<div class="thumbnail">
+							<div class="caption">
+								<h4>Janak Puri</h4>
+								<p><Strong>Arya Samaj Mandir, D Block, Janam Puri, New Delhi</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+				<div class="col-sm-4">
+						<div class="thumbnail">
+							<div class="caption">
+								<h4>Sagar Pur</h4>
+								<p><Strong>New Gian Public School,Shiv Puri, West Sagar Pur, New Delhi</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221 </p>
+								
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+				<div class="col-sm-4">
+						<div class="thumbnail">
+							<div class="caption">
+								<h4>Panjabi Bagh</h4>
+								<p><Strong>Panjabi Bagh,Behind Panjabi Bagh Club, New Delhi</strong></p>
+								<p><strong>Contact:</strong>+918888819478 / 9075555221</p>
+								
+							</div><!--end caption-->
+						</div><!--thumbnail ends here-->
+				</div><!-- Col-4 ends here -->
+				
+			</div>
+</div>
+
+	<?php include('common/footer.php');?>
+		
+</body>
+</html>
